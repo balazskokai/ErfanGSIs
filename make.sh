87b0dbba6cb455d0fe4b16a4ee8d33830ae5b70e
Shahin Sharkar <techharz@gmail.com>
2021-04-23 18:13:01 +0600

    Update make.sh

--- make.sh
+++ make.sh
@@ -194,7 +194,7 @@ if [ "$outputtype" == "Aonly" ]; then
 fi
 
 date=`date +%Y%m%d`
-outputname="$romtypename-$outputtype-$sourcever-$date-nippongsi"
+outputname="$romtypename-$outputtype-$sourcever-$date-kangergsi"
 outputimagename="$outputname".img
 outputtextname="$outputname".txt
 if [ "$4" == "" ]; then
