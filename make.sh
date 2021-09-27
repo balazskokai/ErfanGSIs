e62fcc55a12b97eaea87e0f32c27232533c536ed
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2021-09-27 16:59:02 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -194,7 +194,7 @@ if [ "$outputtype" == "Aonly" ]; then
 fi
 
 date=`date +%Y%m%d`
-outputname="$romtypename-$outputtype-$sourcever-$date-anantgsi"
+outputname="$romtypename-$outputtype-$sourcever-$date-Anantgsi"
 outputimagename="$outputname".img
 outputtextname="$outputname".txt
 if [ "$4" == "" ]; then
@@ -219,7 +219,7 @@ elif [[ $(grep "ro.build.id" $systemdir/system/build.prop) ]]; then
 fi
 displayid2=$(echo "$displayid" | sed 's/\./\\./g')
 bdisplay=$(grep "$displayid" $systemdir/system/build.prop | sed 's/\./\\./g; s:/:\\/:g; s/\,/\\,/g; s/\ /\\ /g')
-sed -i "s/$bdisplay/$displayid2=Ported\.by\.anantgsi\.using\.ErfanTools/" $systemdir/system/build.prop
+sed -i "s/$bdisplay/$displayid2=Ported\.by\.Anantgsi\.using\.ErfanTools/" $systemdir/system/build.prop
 
 # Getting system size and add approximately 5% on it just for free space
 systemsize=`du -sk $systemdir | awk '{$1*=1024;$1=int($1*1.05);printf $1}'`
