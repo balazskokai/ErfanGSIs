25c194017b0c08a7053e070ba3770adc09b8de41
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2021-10-14 11:21:18 +0530

    Add files via upload

--- roms/12/Pixel/make.sh
+++ roms/12/Pixel/make.sh
@@ -3,11 +3,14 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
-# AOSP libs
-cp -fpr $thispath/overlay/* $1/product/overlay/
+# Copy changes in system folder
+rsync -ra $thispath/system/ $1/
+# Fix overlays
+cat $thispath/rw-system.add.sh >> $1/bin/rw-system.sh
 
-# Append file_context
-echo "ro.boot.vendor.overlay.theme=com.google.android.theme.pixel" >> $1/product/etc/build.prop
+# Append usefull stuff
+echo "ro.support_one_handed_mode=true" >> $1/build.prop
+echo "ro.boot.vendor.overlay.theme=com.google.android.systemui.gxoverlay" >> $1/product/etc/build.prop
 echo "ro.config.ringtone=The_big_adventure.ogg" >> $1/product/etc/build.prop
 echo "ro.config.notification_sound=Popcorn.ogg" >> $1/product/etc/build.prop
 echo "ro.config.alarm_alert=Bright_morning.ogg" >> $1/product/etc/build.prop
@@ -20,9 +23,9 @@ echo "ro.com.google.clientidbase=android-google" >> $1/product/etc/build.prop
 echo "ro.error.receiver.system.apps=com.google.android.gms" >> $1/product/etc/build.prop
 echo "ro.com.google.ime.theme_id=5" >> $1/product/etc/build.prop
 echo "ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms" >> $1/product/etc/build.prop
+echo "ro.com.google.ime.height_ratio=1.0" >> $1/product/etc/build.prop
 
+# Drop some services
 sed -i "/dataservice_app/d" $1/product/etc/selinux/product_seapp_contexts
 sed -i "/dataservice_app/d" $1/system_ext/etc/selinux/system_ext_seapp_contexts
-
-# Drop HbmSVManager which is crashing light hal
-rm -rf $1/system_ext/priv-app/HbmSVManager
+sed -i "/ro.sys.sdcardfs/d" $1/product/etc/build.prop
