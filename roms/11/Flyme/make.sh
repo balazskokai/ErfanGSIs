1b7bacb818aef7776727eeb569758651b5712f5d
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2021-10-17 05:07:26 +0530

    Add files via upload

--- roms/11/Flyme/make.sh
+++ roms/11/Flyme/make.sh
@@ -7,12 +7,9 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 echo "ro.bootprof.disable=1" >> $1/build.prop
 
 # Custom files
-cp -fpr $thispath/lib64/* $1/lib64/
 cp -fpr $thispath/erfan $1/
 cp -fpr $thispath/init/* $1/etc/init/
-cp -fpr $thispath/bin/* $1/bin/
 cp -fpr $thispath/overlay/* $1/product/overlay/
-cp -fpr $thispath/framework/* $1/framework/
 
 # hack bootprof
 sed -i "s|/sys/bootprof/bootprof|/system/erfan/bootprof|g" $1/lib/libsurfaceflinger.so
@@ -24,16 +21,5 @@ sed -i "s|/sys/bootprof/bootprof|/system/erfan/bootprof|g" $1/lib64/libsurfacefl
 # cpufreq is running at a high freq
 rm -rf $1/app/com.wolfsonmicro.ez2control
 
-# remove rounded corners
-zip -d $1/framework/flyme-res.apk 'res/*/angular*' 2>/dev/null
-
 # Append file_context
 cat $thispath/file_contexts >> $1/etc/selinux/plat_file_contexts
-
-# Fix Flyme Call
-"$thispath/../../../zip2img.sh" "$FIRMWARE_PATH" "$thispath/../../../working/" "-v"
-vendorpath="$thispath/../../../working/vendor"
-mkdir $vendorpath
-sudo mount $thispath/../../../working/vendor.img $vendorpath
-cp -frp $vendorpath/overlay/FrameworksResCommon.apk $1/product/overlay/VendorFrameworksResCommon.apk
-sudo umount $vendorpath
