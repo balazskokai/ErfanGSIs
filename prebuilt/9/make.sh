e2e22490f0eb1fd7a55a6c500c8dfc8a90e5c43c
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2021-10-17 06:39:59 +0530

    Add files via upload

--- prebuilt/9/make.sh
+++ prebuilt/9/make.sh
@@ -16,12 +16,17 @@ mkdir -p $1/product/overlay
 
 cp -fpr $thispath/nondevice_overlay/* $1/product/overlay/
 
-if [[ ! -f "$romdir/NODEVICEOVERLAY" ]]; then
+if [ -f $romdir/NODEVICEOVERLAY ]; then
+    echo "-> Using device specific overlays is not supported in this rom. Skipping..."
+else
     cp -fpr $thispath/overlay/* $1/product/overlay/
 fi
 
 ## Brightness fix
 # Some systems are using custom light services, don't apply this patch on those roms
-if [[ ! -f "$romdir/DONTPATCHLIGHT" ]]; then
+if [ -f $romdir/DONTPATCHLIGHT ]; then
+    echo "-> Patching lights for brightness fix is not supported in this rom. Skipping..."
+else
+    echo "-> Start Patching Light Services for Brightness Fix..."
     $thispath/brightnessfix/make.sh "$systempath"
 fi
