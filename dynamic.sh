5742d32fe97357d09c42d19673e0597ca9a8b23f
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2021-10-17 07:09:47 +0530

    Add files via upload

---
+++ dynamic.sh
@@ -0,0 +1,486 @@
+#!/bin/bash
+
+# Merge S-P by Velosh @ Treble-Experience
+# License: GPL3
+
+# Main var
+DYNAMIC=false
+
+### Initial vars
+PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+WORKING="$PROJECT_DIR/working"
+
+SYSTEM_NEW_DIR="$WORKING/system_new"
+SYSTEM_NEW_IMAGE="$WORKING/system_new.img"
+SYSTEM_NEW=false
+
+SYSTEM_DIR="$WORKING/system"
+SYSTEM_IMAGE="$WORKING/system.img"
+
+SYSTEM_OTHER_DIR="$WORKING/system_other"
+SYSTEM_OTHER_IMAGE="$WORKING/system_other.img"
+SYSTEM_OTHER=false
+
+PRODUCT_DIR="$WORKING/product"
+PRODUCT_IMAGE="$WORKING/product.img"
+PRODUCT=false
+
+ODM_DIR="$WORKING/odm"
+ODM_IMAGE="$WORKING/odm.img"
+ODM=false
+
+ONEPLUS=false
+RESERVE_DIR="$WORKING/reserve"
+RESERVE_IMAGE="$WORKING/reserve.img"
+INDIA_DIR="$WORKING/india"
+INDIA_IMAGE="$WORKING/india.img"
+OPPRODUCT_DIR="$WORKING/opproduct"
+OPPRODUCT_IMAGE="$WORKING/opproduct.img"
+
+SYSTEM_EXT_DIR="$WORKING/system_ext"
+SYSTEM_EXT_IMAGE="$WORKING/system_ext.img"
+SYSTEM_EXT=false
+
+VENDOR_DIR="$WORKING/vendor"
+VENDOR_IMAGE="$WORKING/vendor.img"
+OVERLAYS_VENDOR=false
+
+if [ "${EUID}" -ne 0 ]; then
+   echo "-> Run as root!"
+fi
+
+MOUNT() {
+   if $(sudo mount -o loop "$1" "$2" >/dev/null 2>&1); then
+      GOOD=true
+   elif $(sudo mount -o ro "$1" "$2" >/dev/null 2>&1); then
+      GOOD=true
+   else
+      echo "-> Failed to mount $3 image, abort!"
+      exit 1
+   fi
+}
+
+usage() {
+   echo "Usage: $0 <Path to firmware>"
+   echo -e "\tPath to firmware: the zip!"
+   echo -e "\t--ext: Merge system_ext partition into system"
+   echo -e "\t--odm: Merge odm_feature_list (OnePlus) into system & try to get odm overlays"
+   echo -e "\t--product: Merge product partition into system"
+   echo -e "\t--overlays: Take the overlays from /vendor and put them in a temporary folder and compress at the end of the GSI process"
+   echo -e "\t--oneplus: Merge oneplus partitions into system (OxygenOS/HydrogenOS only)"
+   echo -e "\t--pixel: Merge Pixel partitions into system (Pixel/Generic from Google only)"
+}
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+   key="$1"
+   case $key in
+   --product)
+      PRODUCT=true
+      SYSTEM_NEW=true
+      shift
+      ;;
+   --odm)
+      ODM=true
+      SYSTEM_NEW=true
+      shift
+      ;;
+   --ext)
+      SYSTEM_EXT=true
+      SYSTEM_NEW=true
+      shift
+      ;;
+   --oneplus)
+      ONEPLUS=true
+      SYSTEM_NEW=true
+      shift
+      ;;
+   --pixel)
+      SYSTEM_OTHER=true
+      shift
+      ;;
+   --overlays)
+      OVERLAYS_VENDOR=true
+      shift
+      ;;
+   --help | -h | -?)
+      usage
+      exit
+      ;;
+   *)
+      POSITIONAL+=("$1")
+      shift
+      ;;
+   esac
+done
+set -- "${POSITIONAL[@]}"
+
+if [ ! -d "$WORKING/" ]; then
+   mkdir -p "$WORKING"
+fi
+
+if [[ ! -n $1 ]]; then
+   echo "-> ERROR!"
+   echo " - Enter all needed parameters"
+   rm -rf "$WORKING"
+   usage
+   exit
+fi
+
+bash "${PROJECT_DIR}/zip2img.sh" "$1" "$WORKING"
+
+if [ "$SYSTEM_NEW" == true ]; then
+   if [ -f "$SYSTEM_IMAGE" ]; then
+      if [ -d "$SYSTEM_DIR" ]; then
+         if [ -d "$SYSTEM_DIR/dev/" ]; then
+            sudo umount "$SYSTEM_DIR/"
+         else
+            if [ -d "$SYSTEM_DIR/etc/" ]; then
+               sudo umount "$SYSTEM_DIR/"
+            fi
+         fi
+      fi
+   else
+      echo "-> System Image doesn't exists, abort!"
+      exit 1
+   fi
+fi
+
+if [ "$PRODUCT" == true ]; then
+   if [ -f "$PRODUCT_IMAGE" ]; then
+      if [ -d "$PRODUCT_DIR" ]; then
+         if [ -d "$PRODUCT_DIR/etc/" ]; then
+            sudo umount "$PRODUCT_DIR/"
+         fi
+      fi
+      DYNAMIC=true
+   fi
+fi
+
+if [ $OVERLAYS_VENDOR == true ]; then
+   if [ -f "$VENDOR_IMAGE" ]; then
+      if [ -d "$VENDOR_DIR" ]; then
+         if [ -d "$VENDOR_DIR/etc/" ]; then
+            sudo umount "$VENDOR_DIR/"
+         fi
+      fi
+   fi
+fi
+
+if [ "$ODM" == true ]; then
+   if [ -f "$ODM_IMAGE" ]; then
+      if [ -d "$ODM_DIR" ]; then
+         if [ -d "$ODM_DIR/etc/" ]; then
+            sudo umount "$ODM_DIR/"
+         fi
+      fi
+      DYNAMIC=true
+   fi
+fi
+
+if [ "$ONEPLUS" == true ]; then
+   if [ -f "$OPPRODUCT_IMAGE" ]; then
+      if [ -d "$OPPRODUCT_DIR" ]; then
+         if [ -d "$OPPRODUCT_DIR/etc/" ]; then
+            sudo umount "$OPPRODUCT_DIR/"
+         fi
+      fi
+      DYNAMIC=true
+   fi
+   if [ -f "$RESERVE_IMAGE" ]; then
+      if [ -d "$RESERVE_DIR" ]; then
+         if [ -d "$RESERVE_DIR/*" ]; then
+            sudo umount "$RESERVE_DIR/"
+         fi
+      fi
+      DYNAMIC=true
+   fi
+   if [ -f "$INDIA_IMAGE" ]; then
+      if [ -d "$INDIA_DIR" ]; then
+         if [ -d "$INDIA_DIR/*app*/" ]; then
+            sudo umount "$INDIA_DIR/"
+         fi
+      fi
+      DYNAMIC=true
+   fi
+fi
+
+if [ "$SYSTEM_OTHER" == true ]; then
+   if [ -f "$SYSTEM_OTHER_IMAGE" ]; then
+      if [ -d "$SYSTEM_OTHER_DIR" ]; then
+         if [ -d "$SYSTEM_OTHER_DIR/etc/" ]; then
+            sudo umount "$SYSTEM_OTHER_DIR/"
+         fi
+      fi
+      DYNAMIC=true
+   fi
+fi
+
+if [ "$SYSTEM_EXT" == true ]; then
+   if [ -f "$SYSTEM_EXT_IMAGE" ]; then
+      if [ -d "$SYSTEM_EXT_DIR" ]; then
+         if [ -d "$SYSTEM_EXT_DIR/etc/" ]; then
+            sudo umount "$SYSTEM_EXT_DIR/"
+         fi
+      fi
+      DYNAMIC=true
+   fi
+fi
+
+if [ "$DYNAMIC" == false ]; then
+   echo "-> Abort due non-dynamic firmware or the options were not selected correctly."
+   exit 1
+else
+   if [ "$SYSTEM_NEW" == true ]; then
+      if [ -f "$SYSTEM_NEW_IMAGE" ]; then
+         if [ -d "$SYSTEM_NEW_DIR" ]; then
+            if [ -d "$SYSTEM_NEW_DIR/dev/" ]; then
+               sudo umount "$SYSTEM_NEW_DIR/"
+            fi
+         fi
+
+         sudo rm -rf $SYSTEM_NEW_IMAGE $SYSTEM_NEW_DIR/
+         sudo dd if=/dev/zero of=$SYSTEM_NEW_IMAGE bs=4k count=2048576 >/dev/null 2>&1
+         sudo tune2fs -c0 -i0 $SYSTEM_NEW_IMAGE >/dev/null 2>&1
+         sudo mkfs.ext4 $SYSTEM_NEW_IMAGE >/dev/null 2>&1
+
+         if [ ! -f "$SYSTEM_NEW_IMAGE" ]; then
+            echo " -> System New Image doesn't exists, abort!"
+            exit 1
+         fi
+      else
+         sudo rm -rf $SYSTEM_NEW_IMAGE $SYSTEM_NEW_DIR/
+         sudo dd if=/dev/zero of=$SYSTEM_NEW_IMAGE bs=4k count=2048576 >/dev/null 2>&1
+         sudo tune2fs -c0 -i0 $SYSTEM_NEW_IMAGE >/dev/null 2>&1
+         sudo mkfs.ext4 $SYSTEM_NEW_IMAGE >/dev/null 2>&1
+
+         if [ ! -f "$SYSTEM_NEW_IMAGE" ]; then
+            echo " -> System New Image doesn't exists, abort!"
+            exit 1
+         fi
+      fi
+   fi
+fi
+
+if [ "$SYSTEM_NEW" == true ]; then
+   if [ -f "$SYSTEM_IMAGE" ]; then
+      if [ ! -d "$SYSTEM_DIR/" ]; then
+         mkdir $SYSTEM_DIR
+      fi
+      MOUNT $SYSTEM_IMAGE $SYSTEM_DIR/ "System"
+   fi
+fi
+
+if [ "$SYSTEM_NEW" == true ]; then
+   if [ ! -d "$SYSTEM_NEW_DIR/" ]; then
+      mkdir $SYSTEM_NEW_DIR
+   fi
+   MOUNT $SYSTEM_NEW_IMAGE $SYSTEM_NEW_DIR/ "System New"
+fi
+
+if [ "$PRODUCT" == true ]; then
+   if [ -f "$PRODUCT_IMAGE" ]; then
+      if [ ! -d "$PRODUCT_DIR/" ]; then
+         mkdir $PRODUCT_DIR
+      fi
+      MOUNT $PRODUCT_IMAGE $PRODUCT_DIR/ "Product"
+   fi
+fi
+
+if [ "$ODM" == true ]; then
+   if [ -f "$ODM_IMAGE" ]; then
+      if [ ! -d "$ODM_DIR/" ]; then
+         mkdir $ODM_DIR
+      fi
+      MOUNT $ODM_IMAGE $ODM_DIR/ "ODM"
+   fi
+fi
+
+if [ "$ONEPLUS" == true ]; then
+   if [ -f "$OPPRODUCT_IMAGE" ]; then
+      if [ ! -d "$OPPRODUCT_DIR/" ]; then
+         mkdir $OPPRODUCT_DIR
+      fi
+      MOUNT $OPPRODUCT_IMAGE $OPPRODUCT_DIR/ "Opproduct"
+   fi
+   if [ -f "$RESERVE_IMAGE" ]; then
+      if [ ! -d "$RESERVE_DIR/" ]; then
+         mkdir $RESERVE_DIR
+      fi
+      MOUNT $RESERVE_IMAGE $RESERVE_DIR/ "Reserve"
+   fi
+   if [ -f "$INDIA_IMAGE" ]; then
+      if [ ! -d "$INDIA_DIR/" ]; then
+         mkdir $INDIA_DIR
+      fi
+      MOUNT $INDIA_IMAGE $INDIA_DIR/ "India"
+   fi
+fi
+
+if [ "$SYSTEM_OTHER" == true ]; then
+   if [ -f "$SYSTEM_OTHER_IMAGE" ]; then
+      if [ ! -d "$SYSTEM_OTHER_DIR/" ]; then
+         mkdir $SYSTEM_OTHER_DIR
+      fi
+      MOUNT $SYSTEM_OTHER_IMAGE $SYSTEM_OTHER_DIR/ "System Other"
+   fi
+fi
+
+if [ "$SYSTEM_EXT" == true ]; then
+   if [ -f "$SYSTEM_EXT_IMAGE" ]; then
+      if [ ! -d "$SYSTEM_EXT_DIR/" ]; then
+         mkdir $SYSTEM_EXT_DIR
+      fi
+      MOUNT $SYSTEM_EXT_IMAGE $SYSTEM_EXT_DIR/ "System Ext"
+   fi
+fi
+
+if [ "$OVERLAYS_VENDOR" == true ]; then
+   if [ -f "$VENDOR_IMAGE" ]; then
+      if [ ! -d "$VENDOR_DIR/" ]; then
+         mkdir $VENDOR_DIR
+      fi
+      MOUNT $VENDOR_IMAGE $VENDOR_DIR "Vendor"
+   fi
+fi
+
+if [ "$SYSTEM_NEW" == true ]; then
+   cp -vrp $SYSTEM_DIR/* $SYSTEM_NEW_DIR/ >/dev/null 2>&1 && sync
+   umount $SYSTEM_DIR/
+fi
+
+if [ "$PRODUCT" == true ]; then
+   if [ -f "$PRODUCT_IMAGE" ]; then
+      rm -rf $SYSTEM_NEW_DIR/product $SYSTEM_NEW_DIR/system/product
+      mkdir -p $SYSTEM_NEW_DIR/system/product
+      cp -vrp $PRODUCT_DIR/* $SYSTEM_NEW_DIR/system/product/ >/dev/null 2>&1
+      ln -s /system/product/ $SYSTEM_NEW_DIR/product
+      sync
+   fi
+fi
+
+if [ "$PRODUCT" == true ]; then
+   if [ -f "$PRODUCT_IMAGE" ]; then
+      sudo umount $PRODUCT_DIR/
+   fi
+fi
+
+if [ "$ODM" == true ]; then
+   if [ -f "$ODM_IMAGE" ]; then
+      if [ -f "$ODM_DIR/etc/odm_feature_list" ]; then
+         cp -r "$ODM_DIR/etc/odm_feature_list" "$SYSTEM_NEW_DIR/system/etc/odm_feature_list"
+      fi
+      if [ -d "$ODM_DIR/overlay" ]; then
+         mkdir -p "$WORKING/odmOverlays"
+         cp -vrp $ODM_DIR/overlay/* "$WORKING/odmOverlays" >/dev/null 2>&1
+         rm -rf "$WORKING/odmOverlays/home"
+         tar -zcvf "$WORKING/odmOverlays.gz" "$WORKING/odmOverlays" >/dev/null 2>&1
+         rm -rf $WORKING/odmOverlays/
+         if [ ! -d "$PROJECT_DIR/output/" ]; then
+            mkdir "$PROJECT_DIR/output/"
+         fi
+         mv "$WORKING/odmOverlays.gz" "$PROJECT_DIR/output/.otmp"
+      fi
+   fi
+fi
+
+if [ "$ODM" == true ]; then
+   if [ -f "$ODM_IMAGE" ]; then
+      sudo umount $ODM_DIR/
+   fi
+fi
+
+if [ "$SYSTEM_OTHER" == true ]; then
+   if [ -f "$SYSTEM_OTHER_IMAGE" ]; then
+      cp -vrp $SYSTEM_OTHER_DIR/* system/ >/dev/null 2>&1 && sync
+   fi
+fi
+
+if [ "$SYSTEM_OTHER" == true ]; then
+   if [ -f "$SYSTEM_OTHER_IMAGE" ]; then
+      sudo umount $SYSTEM_OTHER_DIR/
+   fi
+fi
+
+if [ "$ONEPLUS" == true ]; then
+   if [ -f "$OPPRODUCT_IMAGE" ]; then
+      rm -rf $SYSTEM_NEW_DIR/oneplus $SYSTEM_NEW_DIR/system/oneplus
+      mkdir -p $SYSTEM_NEW_DIR/system/oneplus
+      cp -vrp $OPPRODUCT_DIR/* $SYSTEM_NEW_DIR/system/oneplus/ >/dev/null 2>&1
+      ln -s /system/oneplus/ $SYSTEM_NEW_DIR/oneplus
+      sync
+      if [ -f "$RESERVE_IMAGE" ]; then
+         rm -rf $SYSTEM_NEW_DIR/system/reserve
+         mkdir -p $SYSTEM_NEW_DIR/system/reserve
+         cp -vrp $RESERVE_DIR/* $SYSTEM_NEW_DIR/system/reserve/ >/dev/null 2>&1
+         sync
+      fi
+      if [ -f "$INDIA_IMAGE" ]; then
+         rm -rf $SYSTEM_NEW_DIR/system/india
+         mkdir -p $SYSTEM_NEW_DIR/system/india
+         cp -vrp $INDIA_DIR/* $SYSTEM_NEW_DIR/system/india/ >/dev/null 2>&1
+         sync
+      fi
+   fi
+fi
+
+if [ "$ONEPLUS" == true ]; then
+   if [ -f "$OPPRODUCT_IMAGE" ]; then
+      sudo umount $OPPRODUCT_DIR/
+   fi
+   if [ -f "$RESERVE_IMAGE" ]; then
+      sudo umount $RESERVE_DIR/
+   fi
+   if [ -f "$INDIA_IMAGE" ]; then
+      sudo umount $INDIA_DIR/
+   fi
+fi
+
+if [ "$SYSTEM_EXT" == true ]; then
+   if [ -f "$SYSTEM_EXT_IMAGE" ]; then
+      rm -rf $SYSTEM_NEW_DIR/system_ext $SYSTEM_NEW_DIR/system/system_ext
+      mkdir -p $SYSTEM_NEW_DIR/system/system_ext
+      cp -vrp $SYSTEM_EXT_DIR/* $SYSTEM_NEW_DIR/system/system_ext/ >/dev/null 2>&1
+      ln -s /system/system_ext/ $SYSTEM_NEW_DIR/system_ext
+      sync
+   fi
+fi
+
+if [ "$SYSTEM_EXT" == true ]; then
+   if [ -f "$SYSTEM_EXT_IMAGE" ]; then
+      sudo umount $SYSTEM_EXT_DIR/
+   fi
+fi
+
+if [ "$SYSTEM_NEW" == true ]; then
+   sudo umount $SYSTEM_NEW_DIR/
+fi
+
+if [ "$OVERLAYS_VENDOR" == true ]; then
+   if [ -f "$VENDOR_IMAGE" ]; then
+      if [ -d "$VENDOR_DIR/overlay" ]; then
+         mkdir -p "$WORKING/vendorOverlays"
+         cp -vrp $VENDOR_DIR/overlay/* "$WORKING/vendorOverlays" >/dev/null 2>&1
+         rm -rf "$WORKING/vendorOverlays/home"
+         tar -zcvf "$WORKING/vendorOverlays.gz" "$WORKING/vendorOverlays" >/dev/null 2>&1
+         rm -rf $WORKING/vendorOverlays/
+         if [ ! -d "$PROJECT_DIR/output/" ]; then
+            mkdir "$PROJECT_DIR/output/"
+         fi
+         mv "$WORKING/vendorOverlays.gz" "$PROJECT_DIR/output/.tmp"
+      fi
+   fi
+fi
+
+if [ "$OVERLAYS_VENDOR" == true ]; then
+   if [ -f "$VENDOR_IMAGE" ]; then
+      sudo umount $VENDOR_DIR/
+   fi
+fi
+
+mv $WORKING/system_new.img $WORKING/system.tmp
+sudo rm -rf $SYSTEM_DIR $SYSTEM_NEW_DIR $PRODUCT_DIR $SYSTEM_IMAGE $SYSTEM_OTHER_DIR $SYSTEM_OTHER_IMAGE $PRODUCT_IMAGE $SYSTEM_EXT_DIR $SYSTEM_EXT_IMAGE $OPPRODUCT_DIR $OPPRODUCT_IMAGE $ODM_DIR $ODM_IMAGE $VENDOR_DIR
+
+if [ "$SYSTEM_NEW" == true ]; then
+   mv $WORKING/system.tmp $WORKING/system.img
+fi
--- url2GSI.sh
+++ url2GSI.sh
@@ -3,22 +3,58 @@
 # Project OEM-GSI Porter by Erfan Abdi <erfangplus@gmail.com>
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-TOOLS_DIR="$PROJECT_DIR/tools"
-PARTITIONS="system vendor cust odm oem factory product xrom systemex system_ext reserve india"
 
 AB=true
 AONLY=true
 MOUNTED=false
-CLEAN=true
+NOVNDK=false
+CLEAN=false
+DYNAMIC=false
+LOCK="$PROJECT_DIR/cache/.lock"
+DL="${PROJECT_DIR}/scripts/downloaders/dl.sh"
+
+echo "-> Warning: This fork came through VeloshGSIs, originally ErfanGSIs."
+echo " - You can edit the tool but read the NOTICE/LICENSE!"
+
+if [ $(uname) == Darwin ]; then
+    echo "-> Darwin is no longer supported."
+    exit 1
+fi
+
+if [ "${EUID}" -ne 0 ]; then
+    echo "-> Run as root!"
+    exit 1
+fi
+
+if [ -f "$LOCK" ]; then
+    echo "-> Stop, wait for the other job to finish before you can start another one."
+    exit 1
+else
+    mkdir -p "$PROJECT_DIR/cache/"
+    touch "$LOCK"
+    echo "-> Making patch: Cleaning and removing folders that are used to make GSI to avoid problems"
+    if [ -d "$PROJECT_DIR/working/system/" ]; then
+        sudo umount "$PROJECT_DIR/working/system/" > /dev/null 2>&1
+    fi
+    if [ -d "$PROJECT_DIR/working/vendor/" ]; then
+        sudo umount "$PROJECT_DIR/working/vendor/" > /dev/null 2>&1
+    fi
+    if [ -d "$PROJECT_DIR/tools/ROM_resigner/tmp/" ]; then
+        sudo rm -rf "$PROJECT_DIR/tools/ROM_resigner/tmp/"
+    fi
+    sudo rm -rf "$PROJECT_DIR/cache/" "$PROJECT_DIR/tmp/" "$PROJECT_DIR/working/"
+fi
 
 usage()
 {
-    echo "Usage: [--help|-h|-?] [--ab|-b] [--aonly|-a] [--mounted|-m] [--cleanup|-c] $0 <Firmware link> <Firmware type> [Other args]"
+    echo "Usage: [--help|-h|-?] [--ab|-b] [--aonly|-a] [--mounted|-m] [--cleanup|-c] [--dynamic|-d] [--no-vndks|-nv] $0 <Firmware link> <Firmware type> [Other args]"
     echo -e "\tFirmware link: Firmware download link or local path"
     echo -e "\tFirmware type: Firmware mode"
     echo -e "\t--ab: Build only AB"
     echo -e "\t--aonly: Build only A-Only"
     echo -e "\t--cleanup: Cleanup downloaded firmware"
+    echo -e "\t--dynamic: Use this option only if the firmware contains dynamic partitions"
+    echo -e "\t--novndk: Do not include extra VNDK"
     echo -e "\t--help: To show this info"
 }
 
@@ -42,6 +78,14 @@ case $key in
     CLEAN=true
     shift
     ;;
+    --no-vndks|-nv)
+    NOVNDK=true
+    shift
+    ;;
+    --dynamic|-d)
+    DYNAMIC=true
+    shift
+    ;;
     --help|-h|-?)
     usage
     exit
@@ -55,7 +99,9 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 if [[ ! -n $2 ]]; then
-    echo "ERROR: Enter all needed parameters"
+    echo "-> ERROR!"
+    echo " - Enter all needed parameters"
+    sudo rm -rf "$PROJECT_DIR/cache/" "$LOCK"
     usage
     exit
 fi
@@ -77,43 +123,45 @@ DOWNLOAD()
 {
     URL="$1"
     ZIP_NAME="$2"
-    echo "Downloading firmware to: $ZIP_NAME"
-    aria2c -x16 -j$(nproc) -U "Mozilla/5.0" -d "$PROJECT_DIR/input" -o "$ACTUAL_ZIP_NAME" ${URL} || wget -U "Mozilla/5.0" ${URL} -O "$ZIP_NAME"
+    echo "-> Downloading firmware to: $ZIP_NAME"
+    if echo "${URL}" | grep -q "mega.nz\|mediafire.com\|drive.google.com"; then
+        ("${DL}" "${URL}" "$PROJECT_DIR/input" "$ACTUAL_ZIP_NAME") || exit 1
+    else
+        if echo "${URL}" | grep -q "1drv.ms"; then URL=${URL/ms/ws}; fi
+        { type -p aria2c > /dev/null 2>&1 && aria2c -x16 -j$(nproc) -U "Mozilla/5.0" -d "$PROJECT_DIR/input" -o "$ACTUAL_ZIP_NAME" ${URL} > /dev/null 2>&1; } || { wget -U "Mozilla/5.0" ${URL} -O "$PROJECT_DIR/input/$ACTUAL_ZIP_NAME" > /dev/null 2>&1 || exit 1; }
+        aria2c -x16 -j$(nproc) -U "Mozilla/5.0" -d "$PROJECT_DIR/input" -o "$ACTUAL_ZIP_NAME" ${URL} > /dev/null 2>&1 || {
+            wget -U "Mozilla/5.0" ${URL} -O "$PROJECT_DIR/input/$ACTUAL_ZIP_NAME" > /dev/null 2>&1 || exit 1
+        }
+    fi
 }
 
 MOUNT()
 {
-    for p in $PARTITIONS; do
-        if [[ -e "$1/$p.img" ]]; then
-            mkdir -p "$1/$p"
-            printf "$p " >> "$1/mounted.txt"
-            if [ $(uname) == Linux ]; then
-                sudo mount -o ro "$1/$p.img" "$1/$p"
-            elif [ $(uname) == Darwin ]; then
-                fuse-ext2 "$1/$p.img" "$1/$p"
-            fi
-        fi
-    done
+    mkdir -p "$PROJECT_DIR/working/$2"
+    if `sudo mount -o ro "$PROJECT_DIR/working/$1" "$PROJECT_DIR/working/$2" > /dev/null 2>&1`; then
+        echo "-> $3 image successfully mounted"
+    elif `sudo mount -o loop "$PROJECT_DIR/working/$1" "$PROJECT_DIR/working/$2" > /dev/null 2>&1`; then
+        echo "-> $3 image successfully mounted"
+    else
+        # If it fails again, abort
+        echo "-> Failed to mount $3 image, try to check this manually"
+        exit 1
+    fi
 }
 
 UMOUNT()
 {
-    for p in $PARTITIONS; do
-        if [[ -e "$1/$p.img" ]]; then
-            sudo umount "$1/$p"
-        fi
-    done
+    sudo umount "$1"
 }
 
 LEAVE()
 {
-    UMOUNT "$PROJECT_DIR/working"
+    UMOUNT "$PROJECT_DIR/working/system"
+    UMOUNT "$PROJECT_DIR/working/vendor" > /dev/null 2>&1
     rm -rf "$PROJECT_DIR/working"
+    exit 1
 }
 
-echo "Updating tools"
-"$PROJECT_DIR"/update.sh &> /dev/null
-echo "Create Temp and out dir"
 # Create input & working directory if it does not exist
 mkdir -p "$PROJECT_DIR/input" "$PROJECT_DIR/working" "$PROJECT_DIR/output"
 
@@ -128,45 +176,45 @@ if [ $MOUNTED == false ]; then
         RANDOMM=$(echo $RANDOM)
         ACTUAL_ZIP_NAME="$RANDOMM"_FIRMWARE.tgz
         ZIP_NAME="$PROJECT_DIR"/input/"$RANDOMM"_FIRMWARE.tgz
-        echo "Downloading Firmware"
-        DOWNLOAD "$URL" "$ZIP_NAME" &> /dev/null
+        DOWNLOAD "$URL" "$ZIP_NAME"
         URL="$ZIP_NAME"
     fi
-    echo "Extracting Firmware"
-    $TOOLS_DIR/Firmware_extractor/extractor.sh "$URL" "$PROJECT_DIR/working" &> /dev/null || exit 1
+    if [ "$DYNAMIC" == true ]; then
+       "$PROJECT_DIR"/dynamic.sh "$URL" --odm --product --ext --oneplus --overlays --pixel
+    elif [ $DYNAMIC == false ] ; then
+       "$PROJECT_DIR"/zip2img.sh "$URL" "$PROJECT_DIR/working" || exit 1
+    fi
     if [ $CLEAN == true ]; then
         rm -rf "$ZIP_NAME"
     fi
-    MOUNT "$PROJECT_DIR/working"
-    URL="$PROJECT_DIR/working"
-    
+    if [ -f "$PROJECT_DIR/working/system.img" ]; then
+        MOUNT "system.img" "system" "System"
+        URL="$PROJECT_DIR/working/system"
+        if [ -f "$PROJECT_DIR/working/vendor.img" ]; then
+            if [ ! -f "$PROJECT_DIR/output/.tmp" ]; then
+                MOUNT "vendor.img" "vendor" "Vendor"
+            fi
+        fi
+    else
+        echo "-> Error, System Image doesn't exists, abort!"
+        exit 1
+    fi
 fi
 
 if [ $AB == true ]; then
-   "$PROJECT_DIR"/make.sh "${URL}" "${SRCTYPE}" AB "$PROJECT_DIR/output" ${@} || LEAVE
+   "$PROJECT_DIR"/make.sh "${URL}" "${SRCTYPE}" AB ${NOVNDK} "$PROJECT_DIR/output" ${@} || LEAVE
+fi
+
+if [ -d "$PROJECT_DIR/tools/ROM_resigner/tmp/" ]; then
+   sudo rm -rf "$PROJECT_DIR/tools/ROM_resigner/tmp/"
 fi
 
 if [ $AONLY == true ]; then
-    "$PROJECT_DIR"/make.sh "${URL}" "${SRCTYPE}" Aonly "$PROJECT_DIR/output" ${@} || LEAVE
+    "$PROJECT_DIR"/make.sh "${URL}" "${SRCTYPE}" Aonly ${NOVNDK} "$PROJECT_DIR/output" ${@} || LEAVE
 fi
 
-echo "Porting GSI done on: output"
+UMOUNT "$PROJECT_DIR/working/system"
+UMOUNT "$PROJECT_DIR/working/vendor" > /dev/null 2>&1
+rm -rf "$PROJECT_DIR/working"
 
-if [[ -f "$PROJECT_DIR/private_utils.sh" ]]; then
-    . "$PROJECT_DIR/private_utils.sh"
-    UPLOAD "$PROJECT_DIR/output" ${SRCTYPENAME} ${AB} ${AONLY} "${ORIGINAL_URL}"
-fi
-
-DEBUG=false
-if [ $DEBUG == true ]; then
-echo "AONLY = ${AONLY}"
-echo "AB = ${AB}"
-echo "MOUNTED = ${MOUNTED}"
-echo "URL = ${URL}"
-echo "SRCTYPE = ${SRCTYPE}"
-echo "SRCTYPENAME = ${SRCTYPENAME}"
-echo "OTHER = ${@}"
-echo "ZIP_NAME = ${ZIP_NAME}"
-fi
-
-LEAVE
+echo "-> Porting ${SRCTYPENAME} GSI done on: $PROJECT_DIR/output"
